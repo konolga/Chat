1f27b6ff6f55761e7c2ebe4f4fdfba9e8f6b1581
konolga <olgakon.mail@gmail.com>
2019-05-19 23:41:22 +0300

    socketio

--- src/index.js
+++ src/index.js
@@ -1,13 +1,33 @@
 const express = require('express');
 const path = require('path');
+const http = require('http');
+const socketio = require('socket.io');
+const fs = require('fs');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
 const port1 = 8080
 const port2 = 8081
 
 const publicDirectoryPath = path.join(__dirname,'../public')
 app.use(express.static(publicDirectoryPath));
 
-app.listen(port1,()=>{
+io.on('connection',()=>{
+     console.log('new websocket connection')
+})
+
+
+
+// Start the Server
+server.listen(port1,()=>{
     console.log(`Server is up on port ${port1}!`)
 })
+
+
+
+// Store people in chatroom
+let chatters = [];
+// Store messages in chatroom
+let chat_messages = [];
