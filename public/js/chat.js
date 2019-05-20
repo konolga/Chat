6fd878bb007635cb49c6b16d6f16e0e3cd88b9c4
konolga <olgakon.mail@gmail.com>
2019-05-21 01:59:06 +0300

    event emit

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,4 +1,22 @@
 const socket = io();
-socket.on('roomUpdated',()=>{
+const $messageForm = document.querySelector('#message-form');
+const $messageButton = document.querySelector('#message-button');
+const $messageInput = document.querySelector('#message-input');
 
+socket.on('message',(message)=>{
+console.log(message)
 })
+
+document.querySelector('#message-form').addEventListener('submit',(e)=>{
+    e.preventDefault();
+    $messageButton.setAttribute('disabled', 'disabled')
+    const message = e.target.elements.message.value;
+    socket.emit('sendMessage', message, (status)=>{
+        $messageInput.value='';
+        $messageInput.focus();
+        $messageButton.removeAttribute('disabled')
+        console.log('message status', status)
+    })
+})
+
+
--- src/index.js
+++ src/index.js
@@ -1,33 +1,68 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const path = require('path');
 const http = require('http');
 const socketio = require('socket.io');
 const fs = require('fs');
-
+const redis = require('redis');
 const app = express();
-const server = http.createServer(app);
-const io = socketio(server);
 
-const port1 = 8080
-const port2 = 8081
+
+const io = socketio(server)
+const port1 = process.env.PORT || 8080;
+const port2 = process.env.PORT || 8081;
 
 const publicDirectoryPath = path.join(__dirname,'../public')
 app.use(express.static(publicDirectoryPath));
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+
+
+
+const server = http.createServer(app, onRequest).listen(onRequest);
+http.createServer(app, onRequest_b).listen(port2);
+
+function onRequest (req, res) {
+    if(req.socket.localPort==port1){
+        res.write(`Server is up on port ${port1}!`);
+
+    }
+else   res.write(`Server is up on port ${port2}!`);
+res.end();
+return req.PORT;
+
+}
+
+function onRequest_b (req, res) {
+  res.write(`Server is up on port ${port2}!`);
+  res.end();
+}
+
 
 io.on('connection',()=>{
      console.log('new websocket connection')
 })
 
 
-
-// Start the Server
-server.listen(port1,()=>{
-    console.log(`Server is up on port ${port1}!`)
-})
-
-
-
 // Store people in chatroom
 let chatters = [];
 // Store messages in chatroom
 let chat_messages = [];
+
+io.on('connection',(socket)=>{
+    socket.emit('message', 'Welcome!') //emits the event to the single client
+    
+    socket.broadcast.emit('message', 'a new user joined') //emits the event to every connected client except this paticular client
+
+    socket.on('sendMessage',(message, status)=>{
+        io.emit('message', message) //emits the event to every connected client
+        status('delivered')
+    })
+
+    socket.on('disconnect',()=>{
+        io.emit('message', 'User left')
+    })
+})
